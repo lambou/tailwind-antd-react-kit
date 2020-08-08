f126336687e2801737ee275e71be09c8b4797a55
Arnold LAMBOU <lambouarnold@gmail.com>
2020-08-08 03:06:17 +0100

    Updates

---
+++ precache-manifest.00eda3e627b90cd818f469c07d261eec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa321bdf8e178335d2e90dcf117b65d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bbc7409b9ae8648974a2",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "138a72c518b7284d4a04",
+    "url": "./static/js/2.b4a07786.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b4a07786.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbc7409b9ae8648974a2",
+    "url": "./static/js/main.eaddb1e3.chunk.js"
+  },
+  {
+    "revision": "c9c15678acf970ef5c51",
+    "url": "./static/js/runtime-main.2e9aabe2.js"
+  }
+]);
