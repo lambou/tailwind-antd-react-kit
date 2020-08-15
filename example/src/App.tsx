146426e000c93282aed399d439219d4ff939b842
Arnold LAMBOU <lambouarnold@gmail.com>
2020-08-15 08:01:38 +0100

    ErrorWrapper optimized (mode, loading)

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { ExampleComponent } from 'tailwind-antd-react-kit'
-import { FlexSpace, Container, CButton, ErrorBoundary } from 'tailwind-antd-react-kit'
+import {
+  FlexSpace,
+  Container,
+  CButton,
+  ErrorBoundary,
+  ErrorWrapper
+} from 'tailwind-antd-react-kit'
 import 'tailwind-antd-react-kit/dist/index.css'
 import './index.less'
 import Button from 'antd/lib/button'
@@ -8,14 +14,16 @@ import Button from 'antd/lib/button'
 const App = () => {
   return (
     <ErrorBoundary>
-      <Container>
-        <FlexSpace marginY={true} direction='vertical' justify='center'>
-          <ExampleComponent text='Create React Library Example 😄' />
-          <ExampleComponent text='Create React Library Example 😄' />
-          <CButton className='rounded-full'>H</CButton>
-          <Button className='rounded-full'>Hey</Button>
-        </FlexSpace>
-      </Container>
+      <ErrorWrapper errors={[{ name: 'h', type: 'HTTP' }]} mode='overlay' overlayClassName="z-10" loading>
+        <Container>
+          <FlexSpace marginY={true} direction='vertical' justify='center'>
+            <ExampleComponent text='Create React Library Example 😄' />
+            <ExampleComponent text='Create React Library Example 😄' />
+            <CButton className='rounded-full'>H</CButton>
+            <Button className='rounded-full'>Hey</Button>
+          </FlexSpace>
+        </Container>
+      </ErrorWrapper>
     </ErrorBoundary>
   )
 }
--- src/components/error/ErrorWrapper/index.tsx
+++ src/components/error/ErrorWrapper/index.tsx
@@ -8,9 +8,14 @@ import {
 import { Link } from 'react-router-dom'
 import clsx from 'clsx'
 import IError from '../../../interfaces/IError'
+import Spin, { SpinProps } from 'antd/lib/spin'
 
 export type ErrorWrapperProps = React.HTMLAttributes<HTMLDivElement> & {
   errors: IError[]
+  loading?: boolean
+  spinProps?: Omit<SpinProps, 'spinning'>
+  mode?: 'replace' | 'overlay'
+  overlayClassName?: string
   refreshButtonText?: React.ReactNode
   goToHomePageButtonText?: React.ReactNode
   renderError?: (errors: IError[]) => React.ReactNode
@@ -23,10 +28,18 @@ const ErrorWrapper = React.forwardRef<HTMLDivElement, ErrorWrapperProps>(
       errors,
       renderError,
       className,
+      overlayClassName,
+      loading,
+      spinProps,
+      mode,
       refreshButtonText,
       goToHomePageButtonText,
+      style,
       ...propsRest
     } = props
+
+    // explode style
+    const { backdropFilter, ...styleRest } = style ?? {}
 
     const [internalErrors, setInternalErrors] = useState(errors)
 
@@ -59,86 +72,111 @@ const ErrorWrapper = React.forwardRef<HTMLDivElement, ErrorWrapperProps>(
       setInternalErrors(props.errors)
     }, [props.errors])
 
-    return errors.length !== 0 ? (
-      <div
-        ref={ref}
-        className={clsx([
-          className,
-          'flex items-center justify-center w-full h-full p-8'
-        ])}
-        {...propsRest}
-      >
-        {renderError ? (
-          renderError(internalErrors)
-        ) : (
-          <div className='flex flex-col items-center justify-center'>
-            <WarningOutlined
-              className='text-red-500'
-              style={{ fontSize: '4rem' }}
-            />
-            <div className='text-2xl font-bold text-red-500'>
-              {isInternetAccessProblem(errors)
-                ? 'No internet access'
-                : 'Something went wrong!'}
-            </div>
+    return (
+      <Spin spinning={loading} {...spinProps}>
+        <div className='relative'>
+          {errors.length !== 0 && (
+            <div
+              ref={ref}
+              className={clsx([
+                className,
+                'flex items-center justify-center w-full h-full p-8',
+                mode === 'overlay'
+                  ? ['absolute bg-white bg-opacity-75', overlayClassName]
+                  : ''
+              ])}
+              style={{
+                backdropFilter: backdropFilter ?? 'blur(2.5px)',
+                ...styleRest
+              }}
+              {...propsRest}
+            >
+              {renderError ? (
+                renderError(internalErrors)
+              ) : (
+                <div className='flex flex-col items-center justify-center relative'>
+                  <WarningOutlined
+                    className='text-red-500'
+                    style={{ fontSize: '4rem' }}
+                  />
+                  <div className='text-2xl font-bold text-red-500'>
+                    {isInternetAccessProblem(errors)
+                      ? 'No internet access'
+                      : 'Something went wrong!'}
+                  </div>
 
-            {internalErrors.map((error, index) => {
-              return (
-                <div
-                  key={index}
-                  id={index.toString()}
-                  className='flex items-center flex-col'
-                >
-                  <div className='text-lg py-3'>{getMessage(error)}</div>
-                  {error.refreshCallback ? (
-                    <Button
-                      icon={<ReloadOutlined />}
-                      onClick={() => {
-                        // call refresh callback
-                        if (error.reloadPage === true) {
-                          window.location.reload()
-                        } else {
-                          if (error.refreshCallback) error.refreshCallback()
-                        }
-                      }}
-                      className='rounded-md'
-                      type='primary'
-                    >
-                      {refreshButtonText ?? 'Refresh'}
-                    </Button>
-                  ) : error.type === 'INTERNAL' ? (
-                    <Link to='/'>
-                      <Button
-                        icon={<LeftOutlined />}
-                        onClick={() => {}}
-                        className='rounded-md flex items-center justify-center'
-                        type='primary'
+                  {internalErrors.map((error, index) => {
+                    return (
+                      <div
+                        key={index}
+                        id={index.toString()}
+                        className='flex items-center flex-col'
                       >
-                        {goToHomePageButtonText ?? 'Back to home page'}
-                      </Button>
-                    </Link>
-                  ) : (
-                    <Button
-                      icon={<ReloadOutlined />}
-                      onClick={() => {
-                        window.location.reload()
-                      }}
-                      className='rounded-md'
-                      type='primary'
-                    >
-                      {refreshButtonText ?? 'Refresh'}
-                    </Button>
-                  )}
+                        <div className='text-lg py-3'>{getMessage(error)}</div>
+                        {error.refreshCallback ? (
+                          <Button
+                            icon={<ReloadOutlined />}
+                            onClick={() => {
+                              // call refresh callback
+                              if (error.reloadPage === true) {
+                                window.location.reload()
+                              } else {
+                                if (error.refreshCallback)
+                                  error.refreshCallback()
+                              }
+                            }}
+                            className='rounded-md'
+                            type='primary'
+                          >
+                            {refreshButtonText ?? 'Refresh'}
+                          </Button>
+                        ) : error.type === 'INTERNAL' ? (
+                          <Link to='/'>
+                            <Button
+                              icon={<LeftOutlined />}
+                              onClick={() => {}}
+                              className='rounded-md flex items-center justify-center'
+                              type='primary'
+                            >
+                              {goToHomePageButtonText ?? 'Back to home page'}
+                            </Button>
+                          </Link>
+                        ) : (
+                          <Button
+                            icon={<ReloadOutlined />}
+                            onClick={() => {
+                              window.location.reload()
+                            }}
+                            className='rounded-md'
+                            type='primary'
+                          >
+                            {refreshButtonText ?? 'Refresh'}
+                          </Button>
+                        )}
+                      </div>
+                    )
+                  })}
                 </div>
-              )
-            })}
-          </div>
-        )}
-      </div>
-    ) : (
-      React.createElement('div', null, props.children)
+              )}
+            </div>
+          )}
+          {!(errors.length !== 0 && mode === 'replace') &&
+            React.createElement(
+              'div',
+              {
+                className: mode === 'overlay' ? 'pointer-events-none' : ''
+              },
+              props.children
+            )}
+        </div>
+      </Spin>
     )
   }
 )
+
+ErrorWrapper.defaultProps = {
+  mode: 'replace',
+  loading: false
+}
 
 export default ErrorWrapper
