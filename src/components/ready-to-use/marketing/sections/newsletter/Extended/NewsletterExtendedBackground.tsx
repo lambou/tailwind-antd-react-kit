2e1a12625219c9a2f0daf13189db8303eba64b10
Arnold LAMBOU <lambouarnold@gmail.com>
2021-12-03 11:04:32 +0100

    NewsletterExtendedBackground component added

---
+++ src/components/ready-to-use/marketing/sections/newsletter/Extended/NewsletterExtendedBackground.tsx
@@ -0,0 +1,184 @@
+import clsx from "clsx";
+import React from "react";
+import NewsletterExtended, { NewsletterExtendedProps } from "./NewsletterExtended";
+
+type NewsletterExtendedBackgroundProps = React.HTMLAttributes<HTMLDivElement> & {
+  /**
+   * Newsletter form
+   */
+  formProps?: NewsletterExtendedProps;
+
+  /**
+   * Gap class
+   *
+   * @default `gap-2`
+   */
+  gapClass?: string;
+
+  /**
+   * Padding
+   * when the value is `true`, the tailwind class `p-4` will be applied
+   *
+   * @default true
+   */
+  padding?: boolean | string;
+
+  /**
+   * Rounded
+   * when the value is `true`, the tailwind class `rounded-lg` will be applied
+   *
+   * @default false
+   */
+  rounded?: boolean | string;
+
+  /**
+   * Bordered
+   * when the value is `true`, the tailwind class `border` will be applied
+   *
+   * @default false
+   */
+  bordered?: boolean | string;
+
+  /**
+   * Shadow
+   * when the value is `true`, the tailwind class `shadow-md` will be applied
+   *
+   * @default false
+   */
+  shadow?: boolean | string;
+
+  /**
+   * Image
+   *
+   * @default undefined
+   */
+  backgroundImageUrl?: string;
+
+  /**
+   * Image overlay
+   * @default false
+   */
+  overlay?: boolean;
+
+  /**
+   * Overlay component (`div`) class
+   * @default undefined
+   */
+  overlayClass?: string;
+
+  /**
+   * Image overlay style
+   * @default undefined
+   */
+  overlayStyle?: React.CSSProperties;
+};
+
+const NewsletterExtendedBackground = React.forwardRef<
+  HTMLDivElement,
+  NewsletterExtendedBackgroundProps
+>((props, ref) => {
+  // explode props
+  const {
+    formProps,
+    gapClass,
+    rounded,
+    padding,
+    bordered,
+    shadow,
+    backgroundImageUrl,
+    overlay,
+    overlayStyle,
+    overlayClass,
+
+    /**
+     * Native props
+     */
+    className,
+    style,
+    ...restProps
+  } = props;
+
+  // explode style
+  const { backgroundImage, backgroundSize, ...restStyle } = style ?? {};
+  return (
+    <div
+      ref={ref}
+      className={clsx([
+        className,
+        "flex flex-row overflow-hidden relative",
+        gapClass,
+        typeof bordered === "string" ? bordered : undefined,
+        typeof shadow === "string" ? shadow : undefined,
+        typeof padding === "string" ? padding : undefined,
+        {
+          border: bordered === true,
+          "rounded-lg": rounded === true,
+          "shadow-md": shadow === true,
+          "p-4": padding === true,
+        },
+      ])}
+      style={{
+        backgroundSize: backgroundSize ?? "cover",
+        backgroundImage: backgroundImage ?? `url(${backgroundImageUrl})`,
+        ...restStyle,
+      }}
+      {...restProps}
+    >
+      {overlay === true &&
+        (function () {
+          // overlay style explode
+          const { pointerEvents: pointerEventsOverlay, ...restOverlayStyle } =
+            overlayStyle ?? {};
+          return (
+            <div
+              className={clsx([
+                overlayClass,
+                "absolute inset-0 z-0",
+                {
+                  "bg-primary-500": overlayStyle === undefined,
+                },
+              ])}
+              style={{
+                pointerEvents: pointerEventsOverlay ?? "none",
+                ...restOverlayStyle,
+              }}
+            ></div>
+          );
+        })()}
+      {(() => {
+        // form props
+        const {
+          padding: formPadding,
+          className: formClassName,
+          ...restFormProps
+        } = formProps ?? {};
+        return (
+          <NewsletterExtended
+            className={clsx([
+              formClassName,
+              "flex-auto",
+              { "z-10": overlay === true },
+            ])}
+            padding={formPadding ?? true}
+            {...restFormProps}
+          />
+        );
+      })()}
+    </div>
+  );
+});
+
+NewsletterExtendedBackground.defaultProps = {
+  formProps: undefined,
+  gapClass: undefined,
+  padding: true,
+  rounded: false,
+  bordered: false,
+  shadow: false,
+  backgroundImageUrl: undefined,
+  overlay: false,
+  overlayClass: undefined,
+  overlayStyle: undefined,
+};
+
+export default NewsletterExtendedBackground;
---
+++ src/stories/ready-to-use/marketing/newsletter/extended/NewsletterExtendedBackground.stories.tsx
@@ -0,0 +1,188 @@
+import { MailOutlined } from "@ant-design/icons";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import NewsletterExtendedBackground from "../../../../../components/ready-to-use/marketing/sections/newsletter/Extended/NewsletterExtendedBackground";
+
+export default {
+  title: "Ready-to-use/Marketing/Sections/Newsletter/Extended/Background",
+  component: NewsletterExtendedBackground,
+  argTypes: {
+    className: {
+      type: "string",
+    },
+    gapClass: {
+      type: "string",
+    },
+    padding: {
+      type: "boolean",
+      defaultValue: true,
+    },
+    rounded: {
+      type: "boolean",
+      defaultValue: true,
+    },
+    shadow: {
+      type: "boolean",
+      defaultValue: true,
+    },
+    bordered: {
+      type: "boolean",
+      defaultValue: true,
+    },
+    backgroundImageUrl: {
+      type: "string",
+      defaultValue: undefined,
+    },
+    overlay: {
+      type: "boolean",
+      defaultValue: false,
+    },
+    overlayClass: {
+      type: "string",
+      defaultValue: undefined,
+    },
+    overlayStyle: {
+      type: "object",
+      defaultValue: undefined,
+    },
+  },
+} as ComponentMeta<typeof NewsletterExtendedBackground>;
+
+const Template: ComponentStory<typeof NewsletterExtendedBackground> = (
+  args
+) => {
+  //   const [formInstance] = useForm();
+  return (
+    <div className="container mx-auto">
+      <NewsletterExtendedBackground {...args} />
+    </div>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  className: "text-white",
+  backgroundImageUrl: "https://picsum.photos/500/280",
+  overlayStyle: {
+    backgroundColor: "rgb(0 47 91)",
+    opacity: 0.6,
+  },
+  formProps: {
+    formInputProps: {
+      className: "flex-auto",
+      buttonProps: {
+        className: "rounded-md",
+        size: "large",
+      },
+      inputProps: {
+        className: "rounded-md",
+        size: "large",
+        placeholder: "Enter your email",
+      },
+      errorMessageText: "The email address is required",
+    },
+    centered: true,
+    texts: {
+      title: "Let's get in touch",
+      subTitle: (
+        <div className="font-extrabold text-3xl" style={{ color: "#1890ff" }}>
+          Sign up to our newsletter
+        </div>
+      ),
+      body: "Get the latest news, updates from us",
+      formHelpText:
+        "We care about the protection of your data. Do not forget that.",
+    },
+  },
+};
+
+export const Overlay = Template.bind({});
+Overlay.args = {
+  className: "text-white",
+  backgroundImageUrl: "https://picsum.photos/500/280",
+  overlay: true,
+  overlayStyle: {
+    backgroundColor: "rgb(0 47 91)",
+    opacity: 0.6,
+  },
+  formProps: {
+    formInputProps: {
+      className: "flex-auto",
+      buttonProps: {
+        className: "rounded-md",
+        size: "large",
+      },
+      inputProps: {
+        className: "rounded-md",
+        size: "large",
+        placeholder: "Enter your email",
+      },
+      errorMessageText: "The email address is required",
+    },
+    centered: true,
+    texts: {
+      title: "Let's get in touch",
+      subTitle: (
+        <div className="font-extrabold text-3xl" style={{ color: "#1890ff" }}>
+          Sign up to our newsletter
+        </div>
+      ),
+      body: (
+        <span className="text-xl text-gray-200">
+          Get the latest news, updates from us
+        </span>
+      ),
+      formHelpText: (
+        <span className="text-base text-gray-200">
+          We care about the protection of your data. Do not forget that
+        </span>
+      ),
+    },
+  },
+};
+
+export const Centered = Template.bind({});
+Centered.args = {
+  className: "text-white",
+  backgroundImageUrl: "https://picsum.photos/500/280",
+  overlay: true,
+  overlayStyle: {
+    backgroundColor: "rgb(0 47 91)",
+    opacity: 0.6,
+  },
+  formProps: {
+    className: "text-center",
+    formInputProps: {
+      className: "flex-auto",
+      buttonProps: {
+        className: "rounded-md",
+        size: "large",
+      },
+      inputProps: {
+        className: "rounded-md",
+        size: "large",
+        placeholder: "Enter your email",
+      },
+      errorMessageText: "The email address is required",
+    },
+    layout: "vertical",
+    centered: true,
+    texts: {
+      title: "Let's get in touch",
+      subTitle: (
+        <div className="font-extrabold text-3xl" style={{ color: "#1890ff" }}>
+          Sign up to our newsletter
+        </div>
+      ),
+      body: (
+        <span className="text-xl text-gray-200">
+          Get the latest news, updates from us
+        </span>
+      ),
+      formHelpText: (
+        <span className="text-base text-gray-200">
+          We care about the protection of your data. Do not forget that
+        </span>
+      ),
+    },
+  },
+};
